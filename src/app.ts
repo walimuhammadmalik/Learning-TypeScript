6d9d4aa627beb4e3387f6522dffaf4849bcca4b2
Walii <walimalik111@gmail.com>
2024-08-27 21:28:18 +0500

    add

--- src/app.ts
+++ src/app.ts
@@ -93,7 +93,7 @@ app.post("/delete", async function (req, res) {
     await userRepo.delete(user);
     res.send("User deleted successfully");
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     res.status(500).send("Internal server error");
   }
 });
